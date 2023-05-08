e754b52bef441e2b025e2d50915c50ae28cb0606
SMooreSwe <stevedm88@gmail.com>
2023-05-08 11:26:35 +0200

    fetching data and displaying correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
 
+type Name = {
+  title: string,
+  first: string,
+  last: string
+}
+
+type Street = {
+  number: number,
+  name: string,
+}
+
+type UserLocation = {
+  street: Street,
+  city: string,
+  country: string,
+  postcode: number,
+}
+
+type Age = {
+  age: number
+}
+
+type User = {
+  name : Name,
+  location: UserLocation,
+  dob: Age,
+}
+
 function App() {
+  const [name, setName] = useState<string>('')
+  const [age, setAge] = useState<string>('')
+  const [address, setAddress] = useState<string>('')
+  
+  useEffect(() => {
+    fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then(response => {
+        const userlist : User[] = response.results
+        const user = userlist[0]
+        setName(`${user.name.first} ${user.name.last}`)
+        setAge(`${user.dob.age}`)
+        setAddress(`${user.location.street.number} ${user.location.street.name} \n
+        ${user.location.city} \n
+        ${user.location.postcode} \n
+        ${user.location.country}`)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>This user is...</header>
+      <main>
+        <div>{name}</div>
+        <div>Age: {age}</div>
+        <div>{address}</div>
+      </main>
     </div>
   );
 }
